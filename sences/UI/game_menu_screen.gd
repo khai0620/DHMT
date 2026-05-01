1f35a752492c2efd05335fde2374010c38bb0fad
khai0620 <24023009@vnu.edu.vn>
2026-05-01 10:56:51 +0700

    sanh cho UI (chua xong:

---
+++ sences/UI/game_menu_screen.gd
@@ -0,0 +1,12 @@
+extends CanvasLayer
+
+func _on_start_game_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_save_game_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_exit_game_button_pressed() -> void:
+	pass # Replace with function body.
