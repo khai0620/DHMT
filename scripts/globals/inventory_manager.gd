7d9117810e5b476542447f7bf3d3643fbdd760df
Vuong <24023097@vnu.edu.vn>
2026-05-02 23:04:54 +0700

    fix xong loi ngo cachua

--- scripts/globals/inventory_manager.gd
+++ scripts/globals/inventory_manager.gd
@@ -5,7 +5,6 @@ var inventory: Dictionary = Dictionary()
 signal inventory_changed
 
 func add_collectable(collectable_name: String) -> void:
-	
 	inventory.get_or_add(collectable_name)
 	
 	if inventory[collectable_name] == null:
